a687a88cc3a4bac04c9152caca31c6dd8b5c1f54
tyejaedon <82274559+tyejaedon@users.noreply.github.com>
2025-03-11 23:24:10 +0300

    pew pew

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Footer from "./components/footer";
